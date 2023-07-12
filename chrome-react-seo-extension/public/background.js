71422981d1679160b311d7c712f3a3fa90f164dc
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2023-07-12 15:13:30 -0700

    Popup Box Implemented
    Styling still needs to be converted to CSS. TailwindCSS doesn't work when trying to inject code into activeTabs due to CSPs.

--- chrome-react-seo-extension/public/background.js
+++ chrome-react-seo-extension/public/background.js
@@ -1,16 +1,5 @@
-function createPopupBox(text, x, y) {
-    const popupBox = document.createElement('div');
-    popupBox.textContent = text;
-    popupBox.classList.add('popup-box');
-    popupBox.style.left = `${x}px`;
-    popupBox.style.top = `${y}px`;
-    document.body.appendChild(popupBox);
-}
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'showText') {
-      createPopupBox(request.text, request.x, request.y)
-      console.log(request.text)
-    }
+        console.log(request.text)
+      }
 });
-
